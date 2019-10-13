a6257ed2c4d06fa0a2fe47a13d510d7b2bf71435
susantikcm <susantikcm@yahoo.com>
2019-10-13 14:13:04 +1100

    Updates

---
+++ precache-manifest.1d46468e0fe1501c1f54ec2f565557d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba13a1be4cfc2c54abe1f126c40847b0",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "f818c9dda136d7d2f838",
+    "url": "/Robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5bd3bb298c51ef667cb5",
+    "url": "/Robofriends/static/css/main.26012f1f.chunk.css"
+  },
+  {
+    "revision": "f818c9dda136d7d2f838",
+    "url": "/Robofriends/static/js/2.824c496a.chunk.js"
+  },
+  {
+    "revision": "5bd3bb298c51ef667cb5",
+    "url": "/Robofriends/static/js/main.efeb696b.chunk.js"
+  },
+  {
+    "revision": "2bb2fac80baf4b00cb85",
+    "url": "/Robofriends/static/js/runtime~main.1cd4d11c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
